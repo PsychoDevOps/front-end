e8e58fb964534ba26f27f1e2b38ed596607830f8
Gonzalo Sakuda <59516474+Gsak79@users.noreply.github.com>
2023-05-01 22:15:51 -0500

    Update http-common.js

--- src/core/services/http-common.js
+++ src/core/services/http-common.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:8081/api/v1/",
+    baseURL: "https://psychohelp.azurewebsites.net/api/v1/",
     headers: {"Content-type": "application/json"}
 });
