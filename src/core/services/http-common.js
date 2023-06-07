6ad843d0b9ffb08c3ea56929cbe11b30fd5cf764
Gonzalo Sakuda <59516474+Gsak79@users.noreply.github.com>
2023-06-06 22:58:27 -0500

    Update http to https

--- src/core/services/http-common.js
+++ src/core/services/http-common.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://20.55.113.23:8081/psychohelp/api/v1/",
+    baseURL: "https://20.55.113.23:8443/psychohelp/api/v1/",
     headers: {"Content-type": "application/json"}
 });
