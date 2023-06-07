105db64d2eb2ddf42eb7bc4fa0be9eb520384999
Gonzalo Sakuda <59516474+Gsak79@users.noreply.github.com>
2023-06-06 21:13:13 -0500

    Change http url

--- src/core/services/http-common.js
+++ src/core/services/http-common.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://psychohelp.azurewebsites.net/api/v1/",
+    baseURL: "http://20.55.113.23:8081/psychohelp/api/v1/",
     headers: {"Content-type": "application/json"}
 });
